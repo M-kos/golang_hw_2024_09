3a3b5ee9968a3617b8d23c1ce3f7161d3d928094
Konstantin Melnikov <k.melnikov@velvetech.ru>
2025-01-30 17:35:29 +0300

    test

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	for _, s := range stages {
+		out = s(runStages(out, done))
+	}
+
+	return out
+}
+
+func runStages(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case value, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- value
+			}
+		}
+	}()
+
+	return out
 }
