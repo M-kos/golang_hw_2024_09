deb57d1959ac87c75bc90f9ef2827435f77128ae
Konstantin Melnikov <k.melnikov@velvetech.dev>
2025-02-02 18:46:29 +0300

    solved

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,79 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrFileNOtExist = errors.New("file not exist")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	inFile, err := os.Open(fromPath)
+
+	defer inFile.Close()
+
+	stat, err := inFile.Stat()
+
+	if err != nil {
+		return err
+	}
+
+	if stat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrFileNOtExist
+		}
+
+		return ErrUnsupportedFile
+	}
+
+	_, err = inFile.Seek(offset, io.SeekStart) 
+
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 {
+		limit = stat.Size()
+	}
+
+	fmt.Println("Limit: ", limit)
+
+	err = writeToFile(inFile, toPath, offset, limit)
+
+	return nil
+}
+
+func writeToFile(inFile io.Reader, toPath string, offset, limit int64) error {
+	outFile, err := os.Create(toPath)
+
+	if err != nil {
+		return err
+	}
+
+	bar := pb.Start64(limit)
+	barWriter := bar.NewProxyWriter(outFile)
+
+	_, err = io.CopyN(barWriter, inFile, limit)
+	bar.Finish()
+
+	if err != nil {
+		if err == io.EOF {
+			return nil
+		}
+
+		return err
+	}
+
 	return nil
 }
